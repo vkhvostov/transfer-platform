f3d60311c46111909b32bea413c54c329d6e9fa9
Viktor Khvostov <viktor.khvostov@gmail.com>
2018-09-19 01:00:31 +0200

    Added more logs

--- src/main/kotlin/test/interview/TransferPlatform.kt
+++ src/main/kotlin/test/interview/TransferPlatform.kt
@@ -23,15 +23,17 @@ object TransferPlatform {
     fun main(args: Array<String>) {
         val configs = Configurations()
         val configFileName = System.getenv("CONFIG_FILE_NAME") ?: "config.properties"
+        logger.info("Config file name is $configFileName")
         val properties = configs.properties(configFileName)
         val appConfig = AppConfig.getInstance(properties)
         val accountService = AccountService.getInstance(accounts)
         val transferService = TransferService.getInstance(accountService)
+        logger.info("Configuration is initialized")
 
         val host = appConfig.appHost
         val port = appConfig.appPort
 
-        logger.info("Staring up the server...")
+        logger.info("Staring up the server on $host:$port ...")
         val baseUri = UriBuilder.fromUri(host).port(port).build()
         val config = ResourceConfig(TransferResource::class.java, AccountResource::class.java)
         val server = JdkHttpServerFactory.createHttpServer(baseUri, config)
