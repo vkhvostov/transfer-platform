6b9f70e3325c519d487ba24d59be03a45c009c3e
Viktor Khvostov <viktor.khvostov@gmail.com>
2018-09-19 15:33:04 +0200

    Additional tests for account controller

--- src/test/kotlin/test/interview/AccountControllerTest.kt
+++ src/test/kotlin/test/interview/AccountControllerTest.kt
@@ -28,7 +28,7 @@ class AccountControllerTest {
     private val gson = Gson()
 
     @Test
-    fun `Successfully creates account from account create request`() {
+    fun `Successfully creates an account from an account create request`() {
         val holder = "Chris Haslam"
         val balance = 500
         val currency = "EUR"
@@ -49,7 +49,22 @@ class AccountControllerTest {
     }
 
     @Test
-    fun `Successfully receive account balance`() {
+    fun `Unsuccessful attempt to create an account due to an incorrect currency code`() {
+        val holder = "Chris Haslam"
+        val balance = 500
+        val currency = "XYZ"
+        val request = "{\n" +
+                "\t\"account_holder\" : \"$holder\",\n" +
+                "\t\"initial_balance\" : $balance,\n" +
+                "\t\"currency\" : \"$currency\"\n" +
+                "}"
+        val response = accountController.createAccount(request)
+
+        Assert.assertEquals(Response.Status.BAD_REQUEST.statusCode, response.status)
+    }
+
+    @Test
+    fun `Successfully receives an account balance`() {
         val balance = "300"
         val accountCode = UUID.randomUUID()
         val account = Account(accountCode, "Ryan Sheckler", BigDecimal(balance), Currency.getInstance("EUR"), AccountStatus.OPEN, listOf("555555"))
@@ -64,7 +79,15 @@ class AccountControllerTest {
     }
 
     @Test
-    fun `Successfully changes account balance`() {
+    fun `Unsuccessfully attempt to receive ab account balance due to an incorrect account code`() {
+        val accountCode = UUID.randomUUID()
+        val response = accountController.receiveAccountBalance(accountCode.toString())
+
+        Assert.assertEquals(Response.Status.BAD_REQUEST.statusCode, response.status)
+    }
+
+    @Test
+    fun `Successfully changes an account balance`() {
         val accountCode = UUID.randomUUID()
         val holder = "Ryan Sheckler"
         val currency = Currency.getInstance("EUR")
@@ -91,7 +114,24 @@ class AccountControllerTest {
     }
 
     @Test
-    fun `Successfully closes account`() {
+    fun `Unsuccessful attempt to change an account balance due to a incorrect account code`() {
+        val accountCode = UUID.randomUUID()
+        val tan = "555555"
+
+        val newBalance = "200"
+        val request = "{\n" +
+                "\t\"account_code\" : \"$accountCode\",\n" +
+                "\t\"balance\" : $newBalance,\n" +
+                "\t\"TAN\" : \"$tan\",\n" +
+                "\t\"note\" : \"Just a balance change\"\n" +
+                "}"
+        val response = accountController.changeAccountBalance(request)
+
+        Assert.assertEquals(Response.Status.BAD_REQUEST.statusCode, response.status)
+    }
+
+    @Test
+    fun `Successfully closes an account`() {
         val accountCode = UUID.randomUUID()
         val holder = "Ryan Sheckler"
         val currency = Currency.getInstance("EUR")
@@ -113,5 +153,19 @@ class AccountControllerTest {
         Assert.assertEquals(balance, actualAccount.balance)
         Assert.assertEquals(currency, actualAccount.currency)
         Assert.assertEquals(AccountStatus.CLOSED, actualAccount.status)
+    }
+
+    @Test
+    fun `Unsuccessful attempt to close an account due to incorrect account code`() {
+        val accountCode = UUID.randomUUID()
+        val tan = "555555"
+
+        val request = "{\n" +
+                "\t\"account_code\" : \"$accountCode\",\n" +
+                "\t\"TAN\" : \"$tan\"\n" +
+                "}"
+        val response = accountController.closeAccount(request)
+
+        Assert.assertEquals(Response.Status.BAD_REQUEST.statusCode, response.status)
     }
 }
